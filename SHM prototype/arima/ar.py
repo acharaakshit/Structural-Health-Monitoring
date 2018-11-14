b407d43ed4598ff3bf21438941447f867f89f7ca
Akshit Achara <29024726+acharaakshit@users.noreply.github.com>
2018-11-14 17:17:19 +0530

    Add files via upload

---
+++ SHM prototype/arima/ar.py
@@ -0,0 +1,33 @@
+from statsmodels.tsa.arima_model import AR
+from pandas import read_csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def main():
+	filepath=os.path.abspath(os.curdir) 
+	series = read_csv(filepath+"/2.csv", header=0, index_col=0, squeeze=True)
+	series.columns = ['a', 'b', 'c', 'd']
+	series['e'] = pow((series['a']*series['a'] + series['b']*series['b'] + series['c']*series['c']), 0.5)
+
+	df = pd.DataFrame({'$a': series['e']})
+	#print(df)
+
+	fd = np.array(df)
+	plt.plot(fd,'--*b')
+
+
+
+	model = AR(fd)
+	results = model.fit()
+	r = results.predict()
+	plt.plot(r,'--*r')
+	plt.legend(['original','smoothed'], loc=0)
+	plt.grid(True)
+	plt.xlabel("Indices",fontsize=12)
+	plt.ylabel("Data",fontsize=12)
+	plt.show()
+
+if __name__ == '__main__':
+	main()
